2f683958a4b3b03e29aa95260b8667e88c29a92b
Vyshnavi-abc <23pa1a1290@vishnu.edu.in>
2026-05-06 22:10:44 +0530

    Update queries.sql

--- Week 1/Day 1/queries.sql
+++ Week 1/Day 1/queries.sql
@@ -1,1 +1,9 @@
+-- 1. Select all columns from the employee table
 
+SELECT * 
+FROM Employee;
+
+ -- 2. Select only the name and salary columns from the employee table.
+
+SELECT name, salary
+FROM Employee;
