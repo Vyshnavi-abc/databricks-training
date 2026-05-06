9cd3cd0c7cecc20a4de6a1d46c2b4b7df63ffb6c
Vyshnavi-abc <23pa1a1290@vishnu.edu.in>
2026-05-06 22:24:52 +0530

    Add SQL queries for employee and department data

--- Week 1/Day 1/queries.sql
+++ Week 1/Day 1/queries.sql
@@ -1,4 +1,4 @@
--- 1. Select all columns from the employee table
+-- 1. Select all columns from the employee table.
 
 SELECT * 
 FROM Employee;
@@ -7,3 +7,22 @@ FROM Employee;
 
 SELECT name, salary
 FROM Employee;
+
+-- 3. Select employees who are older than 30.
+
+SELECT *
+FROM Employee
+WHERE age > 30;
+
+-- 4. Select the names of all departments
+
+SELECT name
+FROM Department;
+
+-- 5. Select employees who work in the IT department.
+
+SELECT e.*
+FROM Employee e
+JOIN Department d
+ON e.department_id = d.department_id
+WHERE d.name = 'IT';
